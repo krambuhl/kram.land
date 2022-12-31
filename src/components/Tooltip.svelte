35e4d40871928d49465ab84783f9126edf40b7fa
Evan Krambuhl <evan.krambuhl@gmail.com>
2022-12-31 10:44:41 -0800

    floater

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
+	import { offset, flip, shift } from '@floating-ui/dom'
+	import { createFloatingActions } from 'svelte-floating-ui'
+
 	export let title: string
+
+	const [floatingRef, floatingContent] = createFloatingActions({
+		strategy: 'absolute',
+		placement: 'top',
+		middleware: [offset(4), flip(), shift({ padding: 12 })]
+	})
+
+	let showTooltip: boolean = false
 </script>
 
-<abbr {title}>
+<span
+	class="underline decoration-dotted"
+	use:floatingRef
+	on:mouseenter={() => (showTooltip = true)}
+	on:mouseleave={() => (showTooltip = false)}
+>
 	<slot />
-</abbr>
+</span>
+
+{#if showTooltip}
+	<div
+		class="
+			absolute py-2 px-3 rounded-md
+			text-xs
+			bg-neutral-200 text-black 
+			dark:bg-neutral-800 dark:text-white
+		"
+		use:floatingContent
+	>
+		{title}
+	</div>
+{/if}
