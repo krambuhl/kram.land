f32f737ac7385c896d2e587f759946a444d224d3
Evan Krambuhl <evan.krambuhl@gmail.com>
2022-12-30 22:34:28 -0800

    just ikea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,23 +2,23 @@
 	export const prerender = true
 </script>
 
-<div class="py-20 px-6 text-center flex flex-col items-center space-y-16">
-	<div class="m-auto max-w-sm grid grid-cols-2 gap-4">
-		<img src="/sequence-1a.png" alt="" class="rounded-lg" />
-		<img src="/sequence-2a.png" alt="" class="rounded-lg" />
-		<img src="/sequence-3a.png" alt="" class="rounded-lg" />
-		<img src="/sequence-4a.png" alt="" class="rounded-lg" />
+<div class="py-20 px-6 text-center flex flex-col items-center space-y-12">
+	<div class="m-auto max-w-[192px]">
+		<img src="/ikea.png" alt="" class="rounded-lg" />
 	</div>
 
 	<div class="m-auto max-w-sm space-y-6">
 		<h1 class="heading-md">oh hi. hello!</h1>
-		<p class="body-md">i’m Evan Krambuhl and i make components with nice people at Patreon</p>
+		<p class="body-md">
+			i’m <b>Evan Krambuhl</b> and i make <b>components and tools</b> with nice people at
+			<b>Patreon</b>
+		</p>
 		<p class="body-md">
 			a lifelong waterdog and outdoorser, i have a love for live music, drumming, generative art and
 			the absurd
 		</p>
 		<p class="body-md">
-			i specialize in high quality ui and thoughtful tools to make developers more productive
+			i specialize in <b>high quality ui</b> and <b>thoughtful tools</b> to make developers more productive
 		</p>
 
 		<p class="body-md">———</p>
