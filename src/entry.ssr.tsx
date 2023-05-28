14007e1bb84ee1e46a1f27b7d19c833de9f4de9d
Evan Krambuhl <evan.krambuhl@gmail.com>
2023-05-28 00:22:32 -0700

    yuck

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -11,17 +11,17 @@
  *
  */
 import {
-  renderToString,
+  // renderToString,
   renderToStream,
   type RenderToStreamOptions,
 } from "@builder.io/qwik/server";
-import { extractCritical } from "@emotion/server";
+// import { extractCritical } from "@emotion/server";
 import { manifest } from "@qwik-client-manifest";
 import Root from "./root";
 
 export default async function (opts: RenderToStreamOptions) {
-  const render = await renderToString(<Root />, { manifest, ...opts });
-  const { css } = extractCritical(render.html);
+  // const render = await renderToString(<Root />, { manifest, ...opts });
+  // const { css } = extractCritical(render.html);
 
   return renderToStream(
     <Root
