afca5dc570e5cd9449e130d265a7d3e222648662
Evan Krambuhl <evan.krambuhl@gmail.com>
2023-05-28 21:29:34 -0700

    mailto

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -59,7 +59,7 @@ export default component$(() => {
                 <Rule />
 
                 <BodyText as="p" size="sm">
-                  say hello: <a href="emailto:evan.krambuhl@gmail.com">email</a>{" "}
+                  say hello: <a href="mailto:evan.krambuhl@gmail.com">email</a>{" "}
                   <a href="./work">portfolio</a>
                 </BodyText>
 
