8428057f233b998fd815e620a033575848241fb7
Evan Krambuhl <evan.krambuhl@gmail.com>
2023-05-28 21:28:06 -0700

    og

--- src/components/router-head/index.tsx
+++ src/components/router-head/index.tsx
@@ -16,6 +16,31 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
+      <meta name="author" content="evan krambuhl" />
+      <meta
+        name="description"
+        content="making internet with the nice people who live there"
+      />
+
+      <meta property="og:url" content="https://kram.land" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="kram.land" />
+      <meta
+        property="og:description"
+        content="making internet with the nice people who live there"
+      />
+      <meta property="og:image" content="https://kram.land/ikea.png" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta property="twitter:domain" content="kram.land" />
+      <meta property="twitter:url" content="https://kram.land" />
+      <meta name="twitter:title" content="kram.land" />
+      <meta
+        name="twitter:description"
+        content="making internet with the nice people who live there"
+      />
+      <meta name="twitter:image" content="https://kram.land/ikea.png" />
+
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
